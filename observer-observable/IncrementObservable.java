b09dc1c3a1df51472b7af8cda2906582860c10ef
John Roberts <roberts.john@gmail.com>
2017-04-04 00:16:54 -0700

    Observable

---
+++ observer-observable/IncrementObservable.java
@@ -0,0 +1,15 @@
+public class IncrementObservable extends Observable {
+  private int counter;
+  public IncrementObservable() {
+    this.counter = 0;
+  }
+
+  public int getIncrement() {
+    return this.counter;
+  }
+
+  public void increment() {
+    this.counter++;
+    this.notifyObservers();
+  }
+}
